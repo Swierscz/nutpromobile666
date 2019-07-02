bb694a1e9973943393d881e5626358c2d5a7d886
Swierscz <jakubborys1@gmail.com>
2019-07-02 11:06:07 +0200

    Multiple permission request added

--- app/src/main/java/pl/wat/nutpromobile/activity/main/Permission.java
+++ app/src/main/java/pl/wat/nutpromobile/activity/main/Permission.java
@@ -15,48 +15,43 @@ import androidx.core.content.ContextCompat;
 import pl.wat.nutpromobile.R;
 
 class Permission {
-    private MainActivity mainActivity;
-
-
-    //https://stackoverflow.com/questions/34040355/how-to-check-the-multiple-permission-at-single-request-in-android-m#
-    //w przypadku implementacji więcej dangerous requestów
-
     Permission(MainActivity mainActivity) {
         this.mainActivity = mainActivity;
     }
 
+    private MainActivity mainActivity;
+
+    //https://stackoverflow.com/questions/34040355/how-to-check-the-multiple-permission-at-single-request-in-android-m#
+    //w przypadku implementacji więcej dangerous requestów
 
 
-
-    void startPermissionRequest(){
-        if(Build.VERSION.SDK_INT >= 23){
-            checkPermission();
-        }else{
+    void startPermissionRequest() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkAllPermissions();
+        } else {
 
         }
     }
 
-    private void checkPermission(){
-        if (ContextCompat.checkSelfPermission(
-                mainActivity,
-                Manifest.permission.ACCESS_FINE_LOCATION
-        )
+    private void checkAllPermissions() {
+        checkPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+        checkPermission(Manifest.permission.READ_CONTACTS);
+    }
+
+    private void checkPermission(String permission) {
+        if (ContextCompat.checkSelfPermission(mainActivity, permission)
                 != PackageManager.PERMISSION_GRANTED
         ) {
             // Permission is not granted // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    mainActivity,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-            )
-            ) {
-                createSecondPermissionExplanationDialog();
+            if (ActivityCompat.shouldShowRequestPermissionRationale(mainActivity, permission)) {
+                createSecondPermissionExplanationDialog(permission);
                 // Show an explanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
                 // sees the explanation, try again to request the permission.
             } else {
                 // No explanation needed, we can request the permission.
                 //J.S Information launched and permission request started after dialog dismiss
-                createPermissionExplanationDialog();
+                createPermissionExplanationDialog(permission);
 
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
@@ -69,47 +64,48 @@ class Permission {
 
 
     void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == mainActivity.getResources().getInteger(R.integer.request_localization_permissions)) {
+
+        if (requestCode == getPermissionRequestCode(permissions[0])) {
             // If request is cancelled, the result arrays are empty.
-            if ((grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+            if ((grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                 // permission was granted, yay! Do the
                 // contacts-related task you need to do.
             } else {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(
-                        mainActivity,
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                )
-                ) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(mainActivity, permissions[0])) {
 
                 } else {
                     createPermissionExplanationDialogAfterNeverAskAgain();
                 }
             }
-
-        }else if(requestCode == 666666){
-            // Add other 'if' lines to check for other
-            // permissions this app might request.
-        }else{
-            //Ignore all other requests
         }
-
     }
 
-    private void createPermissionExplanationDialog(){
-        createPermissionDialog("All permits are necessary for the proper functioning of the application.\n " +
-                        "If you don't grant them, you won't be able to read sensors data etc."
-                , this::requestPermissions);
+    int getPermissionRequestCode(String permission) {
+        if (permission.equals(Manifest.permission.ACCESS_FINE_LOCATION))
+            return mainActivity.getResources().getInteger(R.integer.request_fine_location_permissions);
+        else if (permission.equals(Manifest.permission.READ_CONTACTS)) {
+            return mainActivity.getResources().getInteger(R.integer.request_read_contacts_permisson);
+        } else {
+            return 0;
+        }
     }
 
-    private void createSecondPermissionExplanationDialog(){
-        createPermissionDialog("You've previously canceled permission granting. Please grant all permissins, this time"
-                , this::requestPermissions);
+
+    private void createPermissionExplanationDialog(String permission) {
+        createPermissionDialog("Permission: " + permission + " are necessary for the proper functioning of the application.\n " +
+                        "If you don't grant it, you won't be able to read sensors data etc."
+                , () -> requestPermissions(permission));
     }
 
-    private void createPermissionExplanationDialogAfterNeverAskAgain(){
+    private void createSecondPermissionExplanationDialog(String permission) {
+        createPermissionDialog("You've previously canceled " + permission + " grant. Please do it this time"
+                , () -> requestPermissions(permission));
+    }
+
+    private void createPermissionExplanationDialogAfterNeverAskAgain() {
         final int settingsPermissionCode = mainActivity.getResources().getInteger(R.integer.request_settings_permissions);
-        createPermissionDialog("You've previously check never ask again button, you must change app preferences in settings"
-                ,() ->{
+        createPermissionDialog("You've previously check never ask again button for some permission, you must change app preferences in settings"
+                , () -> {
                     Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
                     Uri uri = Uri.fromParts("package", mainActivity.getPackageName(), null);
                     intent.setData(uri);
@@ -117,7 +113,7 @@ class Permission {
                 });
     }
 
-    private void createPermissionDialog(String message, PermissionBehaviour permissionBehaviour){
+    private void createPermissionDialog(String message, PermissionBehaviour permissionBehaviour) {
         AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
         builder.setMessage(message);
         builder.setNeutralButton("OK", (dialog, which) -> {
@@ -129,17 +125,16 @@ class Permission {
     }
 
 
-
-    interface PermissionBehaviour{
+    interface PermissionBehaviour {
         void work();
     }
 
-    private void requestPermissions(){
-        final int requestCode = mainActivity.getResources().getInteger(R.integer.request_localization_permissions); //TODO przenieść
-        String[] permission = {Manifest.permission.ACCESS_FINE_LOCATION};
+    private void requestPermissions(String permission) {
+        int requestCode = getPermissionRequestCode(permission);
+
 
         ActivityCompat.requestPermissions(mainActivity,
-                permission,
+                new String[]{permission},
                 requestCode
         );
     }
